69ca76557307fb1c808d4de299d43dbd3543a50d
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-13 23:31:58 -0500

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,6 +4,21 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	// parent es el puntero al nodo padre a crear
+	// value es el valor a poner en el nodo
 
-	return (0);
+	binary_tree_t *nnode;
+
+	if (nnode == NULL)
+	{
+		return (NULL);
+	}
+
+	nnode = malloc(sizeof(binary_tree_t));
+
+	nnode->parent = parent;
+	nnode->value = value;
+
+
+	return (nnode);
 }
