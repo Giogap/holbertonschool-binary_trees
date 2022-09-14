c759e55809f6120c3298aa5019a9702187c3b0fe
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-14 00:06:01 -0500

    create binary_tree_print.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 
-
+/**
+ *
+ *
+ *
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	// parent es el puntero al nodo padre a crear
-	// value es el valor a poner en el nodo
-
 	binary_tree_t *nnode;
+
+	nnode = malloc(sizeof(binary_tree_t));
 
 	if (nnode == NULL)
 	{
 		return (NULL);
 	}
 
-	nnode = malloc(sizeof(binary_tree_t));
-
 	nnode->parent = parent;
-	nnode->value = value;
-
+	nnode->n = value;
 
 	return (nnode);
 }
