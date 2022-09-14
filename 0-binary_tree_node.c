040f35c01b05fb7ca5820c18869ef269ef13b8c2
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-14 00:34:14 -0500

    update 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,6 +19,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	nnode->parent = parent;
 	nnode->n = value;
+	nnode->left = NULL;
+	nnode->right = NULL;
 
 	return (nnode);
 }
