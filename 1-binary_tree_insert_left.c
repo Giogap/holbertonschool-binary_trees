c1423d59512082071c010b85eefd6eb835990892
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-14 18:54:58 -0500

    update 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,7 +32,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		else
 		{
 			nnode->left = parent->left;
-			nnode->left = nnode;
+			nnode->left->parent = nnode;
 		}
 	}
 	return (nnode);
