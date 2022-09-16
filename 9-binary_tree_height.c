2a045be1ea1d197f1176d3c9ee8d2e9cb92a9ae4
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-16 14:28:20 -0500

    update 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,28 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		int rDepth = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-		return (MAX(lDepth, rDepth));
+	if (tree == NULL)
+	{
+		return (0);
 	}
-	return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
+	height_l = binary_tree_height(tree->left) + 1;
+	height_r = binary_tree_height(tree->right) + 1;
+
+	if (height_l >= height_r)
+	{
+		return (height_l);
+	}
+	else
+	{
+		return (height_r);
+	}
 }
