f258b6a4359fce3824ef5f4efe5fd3859ed12da3
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-16 14:23:04 -0500

    update 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <sys/param.h>
+
+/**
+ * binary_tree_height - check code
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: always 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		int rDepth = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+		return (MAX(lDepth, rDepth));
+	}
+	return (0);
+}
