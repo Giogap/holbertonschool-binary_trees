6e49df8b4f2f4637253e50f1a0a639c881f28663
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-16 15:21:25 -0500

    update 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - check code
+ * @tree: ointer to the root node of the tree to count the number of leaves
+ * Return: lLeaves + rLeaves or 0 failure
+ *
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
+
+
+
+return (lLeaves + rLeaves);
+}
