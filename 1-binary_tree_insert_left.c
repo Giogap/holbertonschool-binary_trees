372dc7e595a94aed7b0bd5b1657844f7a2e35288
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-15 23:08:17 -0500

    update 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,35 +1,31 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
- *
- *
+ * binary_tree_insert_left - check code
+ * @parent: pointer to the node to insert the left-childin
+ * @value: value to store in the new node
+ * Return: pointer to newNode or NULL on failure
  */
+
+/* elementos menores a la raiz van al lado izq */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nnode;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	nnode = malloc(sizeof(binary_tree_t));
+	nnode = binary_tree_node(parent, value);
 	if (nnode == NULL)
 	{
 		return (NULL);
 	}
-
-	if (value < parent->n && parent != NULL)
+	if (parent->left != NULL)
 	{
-		binary_tree_insert_left(parent->left, value);
+		nnode->left = parent->left;
+		parent->left->parent = nnode;
 	}
-	else
-	{
-		return (NULL);
-	}
+	parent->left = nnode;
 	return (nnode);
 }
