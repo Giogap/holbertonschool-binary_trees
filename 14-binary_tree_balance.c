7d9081a2c4b4f1176c9f078c5d014a4bd38c754f
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-18 22:24:36 -0500

    update 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,18 +7,28 @@
 */
 
 int binary_tree_balance(const binary_tree_t *tree)
-
-
 {
-	int height_l = 0;
-	int height_r = 0;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	if (tree == NULL)
-		return (0);
+	return (0);
+}
 
-	height_l = binary_tree_height(tree->left);
-	height_r = binary_tree_height(tree->right);
+/**
+ * binary_tree_height - check code
+ * @tree: pointer root height
+ * Return: always 0
+ */
 
-	return (height_l - height_r);
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
