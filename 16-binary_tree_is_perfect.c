f0268d176a4ccb5d16766c44da1df19ec520585b
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-18 23:57:58 -0500

    update 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,3 +19,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	return (0);
 }
+
+/**
+ * binary_tree_is_leaf - check code
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if  (node->right == NULL && node->left == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
